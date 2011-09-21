202ac76cebbf917fc7deefdaacb654172b932637
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2011-09-21 06:08:46 +0000

    why were userconfig/standard_dotfiles/.init/shellrc.d/less_setup*.sh still here?
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@78 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/shellrc.d/less_setup.sh
+++
@@ -1,6 +0,0 @@
-# Use the distro's lessopen.sh and lessclose.sh scripts to handle certain files:
-export LESSOPEN="lessopen.sh %s"
-export LESSCLOSE="lessclose.sh %s %s"
-
-export LESS=--LONG-PROMPT
-
--- .init/shellrc.d/less_setup_debian.sh
+++
@@ -1,5 +0,0 @@
-# use a Debian-specific command to filter less' input
-eval `SHELL=/bin/sh lesspipe`
-
-export LESS=--LONG-PROMPT
-
