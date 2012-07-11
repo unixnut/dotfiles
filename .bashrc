ad00a190fa54d2102905fca32c79cff3431de4d0
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2012-07-11 09:16:14 +0000

    userconfig/standard_dotfiles/.init/setup.d/user.sh:   + switched grep test to a case statement   + sets CLICOLOR   + actually checks for vim/vi before setting VISUAL
    userconfig/standard_dotfiles/.init/setup.d/user.sh:
      + fixed -n test for USE_DIRCOLORS
      + removed grep aliases
    
    
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@126 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .bashrc
+++ .bashrc
@@ -61,14 +61,10 @@ case "$TERM" in
 esac
 
 # enable color support of ls and also add handy aliases
-if [ -n $USE_DIRCOLORS ]; then
+if [ -n "$USE_DIRCOLORS" ]; then
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
 fi
 
 # Add an "alert" alias for long running commands.  Use like so:
--- .init/setup.d/user.sh
+++ .init/setup.d/user.sh
@@ -1,5 +1,9 @@
 # -- general setup --
-export VISUAL=vim
+if [ -n "$(type -p vim)" ] ; then
+  export VISUAL=vim
+elif [ -n "$(type -p vi)" ] ; then
+  export VISUAL=vi
+fi
 if [ -d ~/lisp/Zones/$ZONE ] ; then
   export EMACS_ZONEDIR=$HOME/lisp/Zones/$ZONE
 fi
@@ -28,18 +32,28 @@ export CVS_RSH=`which ssh`
 # Midnight Commander needs this to run in color mode
 export COLORTERM=1
 
+# FreeBSD ls needs this to run in color mode
+export CLICOLOR=y
+
+# For GNU ls, see /usr/bin/dircolors test in .init/setup.d/bash.sh
+
 export MOZ_PLUGIN_PATH=/usr/local/lib/browser-plugins
 
 # -- cool grep options --
-export GREP_OPTIONS="--color=auto --exclude=*~ --exclude=#*# --exclude=.*.sw? --exclude=.*.bak"
 # check version number (e.g. GNU grep 2.5.3)
 ver=`grep -V | sed -n '/GNU grep/ s/.* //p'`
-if [ $ver ] ; then
-  # if the version is >= 2.5.3, use dir exclusions
-  if echo $ver | grep -q -e '^2.5.[3-9]' -e '^2.[6-9]' -e '^[3-9]' ; then
-    GREP_OPTIONS="$GREP_OPTIONS --exclude-dir=.svn --exclude-dir=.hg --exclude-dir=.git --exclude-dir=.pc"
-  fi
-fi
+case $ver in
+  2.5.[3-9])
+  2.[6-9].*)
+  [3-9].*)
+    # if the version is >= 2.5.3, use dir exclusions
+    export GREP_OPTIONS="--exclude-dir=.svn --exclude-dir=.hg --exclude-dir=.git --exclude-dir=.pc"
+    # FALLSTHROUGH
+
+  2.5.*)
+    export GREP_OPTIONS="--color=auto --exclude=*~ --exclude=#*# --exclude=.*.sw? --exclude=.*.bak $GREP_OPTIONS"
+    ;;
+esac
 
 # -- X11 setup --
 if [ -d "$HOME/.app-defaults" ] ; then
