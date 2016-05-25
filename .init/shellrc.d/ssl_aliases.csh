8fe4774c9f5a5bff6cba17dedac5725e4ec7f283
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2016-05-25 07:55:59 +0000

    userconfig/standard_dotfiles/.init/shellrc.d/ssl_aliases.{sh,csh}:   + added c_dump
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@306 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/shellrc.d/ssl_aliases.csh
+++ .init/shellrc.d/ssl_aliases.csh
@@ -1,2 +1,2 @@
 alias c_verify 'openssl verify -CApath $SSL_PATH/certs -CAfile "\!:2" "\!:1"'
-
+alias c_dump 'openssl x509 -text -noout -in \!:*' | $PAGER
--- .init/shellrc.d/ssl_aliases.sh
+++ .init/shellrc.d/ssl_aliases.sh
@@ -2,3 +2,8 @@ c_verify()
 {
   openssl verify -CApath $SSL_PATH/certs -CAfile "$2" "$1"
 }
+
+c_dump()
+{
+  openssl x509 -text -noout -in "$@" | ${PAGER:-less}
+}
