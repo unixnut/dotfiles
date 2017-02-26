826430ed0f16b7780bdc76b81a50b00372eb47d5
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2017-02-26 16:26:33 +0000

    Fixed comment in mounts.sh
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@334 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/setup.Linux.d/mounts.sh
+++ .init/setup.Linux.d/mounts.sh
@@ -1,7 +1,7 @@
 
 # If /home is a separate filesystem, check if it is mounted noexec.
 # First interrogate inode number, then mount options.
-# (The root of a filesystem has inode #2, #128 on newer RedHat systems e.g. CentoOS 7.)
+# (The root of an ext* filesystem has inode #2, for xfs it's #128.)
 if expr $(ls -lid /home | cut -d" " -f1) : '^\(2\|128\)$' > /dev/null &&
    expr $(awk '$2 == "/home" { print $4; }' /proc/mounts) : '.*\<noexec\>.*' > /dev/null
 then
