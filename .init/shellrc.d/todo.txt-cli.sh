0bb8414c483ec479cd2f2b8ce4a15edaad9b27e2
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2018-11-19 13:06:25 +0000

    userconfig/standard_dotfiles/.init/shellrc.d/todo.txt-cli.sh: PS1-only
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@390 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/shellrc.d/todo.txt-cli.sh
+++ .init/shellrc.d/todo.txt-cli.sh
@@ -1,13 +1,15 @@
-if [ -f /opt/todo.txt-cli/todo_completion ] ; then
-  . /opt/todo.txt-cli/todo_completion
+if [ -n "$PS1" ] ; then
+  if [ -f /opt/todo.txt-cli/todo_completion ] ; then
+    . /opt/todo.txt-cli/todo_completion
 
-  complete -F _todo t
-elif [ -f /usr/share/bash-completion/completions/todo-txt ] ; then
-  # Force-load this (it's normally autoloaded first time it's needed, but in
-  # this case an alias is being used) so the complete command works
-  . /usr/share/bash-completion/completions/todo-txt
+    complete -F _todo t
+  elif [ -f /usr/share/bash-completion/completions/todo-txt ] ; then
+    # Force-load this (it's normally autoloaded first time it's needed, but in
+    # this case an alias is being used) so the complete command works
+    . /usr/share/bash-completion/completions/todo-txt
 
-  complete -F _todo t
+    complete -F _todo t
+  fi
 fi
 
 alias t=${TODOTXT_CMD-todo.sh}
