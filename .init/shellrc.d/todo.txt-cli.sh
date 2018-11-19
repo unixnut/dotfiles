b561d8b6e7fa0b8fd5250006807d8f2dbd62d217
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2018-11-19 13:03:36 +0000

    Fix to userconfig/standard_dotfiles/.init/shellrc.d/todo.txt-cli.sh
    debian/security.dselect: added 'secure-delete' and 'cryptmount'
    
    debian/e-mail.dselect: Added 'qprint'
    
    debian/sources.{jessie,stretch}/sources.list:
      + Added ftp.debian.org lines
    
    
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@389 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/shellrc.d/todo.txt-cli.sh
+++ .init/shellrc.d/todo.txt-cli.sh
@@ -2,7 +2,7 @@ if [ -f /opt/todo.txt-cli/todo_completion ] ; then
   . /opt/todo.txt-cli/todo_completion
 
   complete -F _todo t
-elif [ $DISTRO = Debian -o $DISTRO_BASE = Debian ] ; then
+elif [ -f /usr/share/bash-completion/completions/todo-txt ] ; then
   # Force-load this (it's normally autoloaded first time it's needed, but in
   # this case an alias is being used) so the complete command works
   . /usr/share/bash-completion/completions/todo-txt
