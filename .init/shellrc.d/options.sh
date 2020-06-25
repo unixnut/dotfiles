02458b5ae17fc53ef15af10747b390423ab25547
alastair <alastair@6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab>
2020-06-25 10:58:37 +0000

    .init/shellrc.d/options.sh: No longer unsets 'extglob'
    git-svn-id: svn+ssh://al.id.au/srv/svn/tools/userconfig/standard_dotfiles@414 6f3dbf8f-7d3b-0410-9ec1-89cb43d465ab

--- .init/shellrc.d/options.sh
+++ .init/shellrc.d/options.sh
@@ -1,5 +1,7 @@
 if [ -n "$BASH_VERSION" ] ; then
 	shopt -s globstar dirspell histreedit no_empty_cmd_completion
 	shopt -u force_fignore
-	shopt -u extglob
+	# Unsetting this breaks completion, e.g. `_filedir_xspec()`
+	# in /usr/share/bash-completion/bash_completion
+	## shopt -u extglob
 fi
